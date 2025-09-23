e21ae302c89302812ae8380608af1896a87ec0ad
Agustín <aguspalacios214@gmail.com>
2025-09-23 02:42:33 -0300

    Player Animation y Crab
    - Se implemento la animacin del cangrejo y se creo un enemigo en la escena

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float Speed = 2;
     public float jumpForce = 3;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
 
     Rigidbody2D rb;
 
@@ -19,18 +21,32 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey("d") || Input.GetKey("right"))
         {
             rb.velocity = new Vector2(Speed, rb.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Movement", true);
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb.velocity = new Vector2(-Speed, rb.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Movement", true);
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
+            animator.SetBool("Movement", false);
         }
         if(Input.GetKey("space") && CheckGround.isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Movement", false);
+        }
+        else
+        {
+            animator.SetBool("Jump", false) ;
         }
     }
 }
